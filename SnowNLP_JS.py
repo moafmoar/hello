e4cf1e5295797beebb087b0453dea47391614593
caomm <cao_manman@126.com>
2018-01-17 11:47:41 +0800

    词频统计

--- SnowNLP_JS.py
+++ SnowNLP_JS.py
@@ -321,6 +321,294 @@ def word_count(k=10):
         "%s(%s);" % (jsonp_callback, json.dumps({'ok': True, 'data': word_dict},ensure_ascii=False)),
         mimetype="text/javascript")
 
+
+################################################ 180115_new start #########################################################################
+@app.route('/word_count_new',methods=['POST','GET'])
+def word_count_new(k=10):
+    '''
+     读取字符串，返回单词和频率数据框,字典结构
+    :param text:
+    :param stopword:
+    :param k 词频排名前k位的
+    :return:返回词和频数的数据框
+    '''
+    jsonp_callback = request.args.get('callback', 'jsonpCallback1')  # 这里的callback对应的值需要和ajax请求的callback保持一致。
+    #text = '读取字符串，返回单词和频率数据框,字典结构'
+    text = request.values['text']
+    print(text)
+    path = 'F:\\easestar\\zstp20180109\\stopwords.txt'
+    stopwords = [word.strip('\n') for word in open(path, 'r', encoding='utf-8').readlines()]
+
+    # word=[]
+    # words=[wd.strip() for wd in jieba.cut(text) if wd not in stopword and wd!='\n' and wd!='\d']
+    '''
+    for word in words:
+        if word not in word_count:
+            word_count[word]=1
+        else:
+            word_count[word] += 1
+    # print(Counter(word))
+    word_count = sorted(word_count.items(), key=lambda d: d[1], reverse=True)
+    word_count1=word_count[:k]
+    word_dict=dict(word_count1)
+    '''
+    #df1 = pseg_cut(text, stopwords=stopword)
+    '''
+     第一步：pseg_cut
+    '''
+    words = [(word, pseg) for word, pseg in pseg.cut(text) if word not in stopwords
+             and word != '\n' and word != '\d']
+
+    df_word1 = pd.DataFrame({'word': [words[i][0] for i in range(len(words))],
+                            'class': [words[i][1] for i in range(len(words))]},
+                           columns=['word', 'class'])
+    df_word1 = df_word1.drop_duplicates()
+    '''
+     第二步：word_count2
+    '''
+    #df2, word_dict = word_count2(text, stopword,10)
+    word_count = dict()
+    # word=[]
+    words2 = [wd.strip() for wd in jieba.cut(text) if wd not in stopwords and wd != '\n' and wd != '\d']
+    for word in words2:
+        if word not in word_count:
+            word_count[word] = 1
+        else:
+            word_count[word] += 1
+    # print(Counter(word))
+    word_count = sorted(word_count.items(), key=lambda d: d[1], reverse=True)
+    if k is not None:
+        word_count1 = word_count[:k]
+    else:
+        word_count1 = word_count
+
+    word_dict = dict(word_count1)
+    df_word2 = pd.DataFrame({'word': [word_count1[i][0] for i in range(len(word_count1))], 'freq': [
+        word_count1[i][1] for i in range(len(word_count1))
+    ]}, columns=['word', 'freq'])
+
+    '''
+     第三步：merge_df
+    '''
+    #df = merge_df(df2, df1)
+    df_word = pd.merge(df_word2, df_word1)
+    df_word.index = range(len(df_word2))
+    df_word['out'] = df_word['word'] + '/' + df_word['class'] + '/' + df_word['freq'].astype(str)
+
+    '''
+     第四步：color_df
+    '''
+    df_cl = color_df(df_word)
+
+    '''
+     第五步：color_df
+    '''
+    df_dict = dict_df(df_cl)  # 你们需要调用的字典形式的返回文件
+
+    print(len(df_dict))
+    for key, valuein in df_dict.items():
+        print(key, valuein)
+
+    '''
+    df_word=pd.DataFrame({'word':[word_count1[i][0]  for i in range(len(word_count1))],
+                          'freq':[word_count1[i][1] for i in range(len(word_count1))]},columns=['word','freq'])
+    '''
+    #return df_word,word_dict
+    return Response(  # return的时候需要通过response返回数据并且将callback一并返回给客户端，这样才能请求成功。
+        "%s(%s);" % (jsonp_callback, json.dumps({'ok': True, 'data': df_dict},ensure_ascii=False)),
+        mimetype="text/javascript")
+
+def pseg_cut(text,stopwords):
+    """
+    给定文本进行分词，标注。返回分词和词性数据框
+    :param text:字符串
+    :return:分词结果和词性列表
+    """
+    words=[(word,pseg) for word,pseg in pseg.cut(text) if word not in stopwords
+           and word !='\n' and word !='\d']
+
+
+    df_word=pd.DataFrame({'word':[words[i][0] for i in range(len(words))],
+                          'class':[words[i][1] for i in range(len(words))]},
+                         columns=['word','class'])
+    df_word=df_word.drop_duplicates()
+
+    return df_word
+def word_count2(text,stopword,k):
+    """
+    读取字符串，返回单词和频率数据框,字典结构
+    :param text:
+    :param stopword:
+    :param k 词频排名前k位的
+    :return:返回词和频数的数据框
+    """
+    word_count=dict()
+    # word=[]
+    words=[wd.strip() for wd in jieba.cut(text) if wd not in stopword and wd!='\n' and wd!='\d']
+    for word in words:
+        if word not in word_count:
+            word_count[word]=1
+        else:
+            word_count[word] += 1
+    # print(Counter(word))
+    word_count = sorted(word_count.items(), key=lambda d: d[1], reverse=True)
+    if k is not None:
+       word_count1=word_count[:k]
+    else:
+        word_count1 = word_count
+
+    word_dict=dict(word_count1)
+    df_word=pd.DataFrame({'word':[word_count1[i][0]  for i in range(len(word_count1))  ],'freq':[
+        word_count1[i][1] for i in range(len(word_count1))
+    ]},columns=['word','freq'])
+
+    return df_word,word_dict
+
+def merge_df(df_word,df_count):
+    df_word=pd.merge(df_word,df_count)
+    df_word.index=range(len(df_word))
+    df_word['out']=df_word['word']+'/'+df_word['class']+'/'+df_word['freq'].astype(str)
+
+    return df_word
+
+
+def color_df(df_word):
+    """"
+    返回每个词性对应的颜色的数据框
+    """
+    color={
+	   'n'   : "#ff8000" ,
+	   'nr'  :"#FFF68F" ,
+	   'nr1' :"#FFEFD5"  ,
+	   'nr2' : "#FFE4E1" ,
+	   'nrj' : "#FFDEAD" ,
+	   'nrf' : "#FFC1C1" ,
+	   'ns'  : "#FFB90F" ,
+	   'nsf' : "#FFA54F" ,
+	   'nt'  : "#FF34B3" ,
+	   'nz'  :"#FF8C00"  ,
+	   'nl'  :"#FF7F50"  ,
+	   'ng'  :"#FF6EB4"  ,
+	   'l'   : "#ff8000" ,
+	   'en'  :"#FF0080"  ,
+	   't'   : "#FF4500" ,
+	   'tg' : "#FF3030"  ,
+	   's'  : "#aaaaff"  ,
+	   'f'  : "#97cbff"  ,
+	   'v'  : "#0080ff"  ,
+	   'vd' : "#C1CDCD"  ,
+	   'vn' : "#BFEFFF"  ,
+	   'an' : "#BDBDBD"  ,
+	  'vyou': "#BC8F8F"  ,
+	   'vf' : "#B9D3EE"  ,
+	   'vx' : "#B5B5B5"  ,
+	   'vi' : "#B3EE3A"  ,
+	  'vl'  : "#B22222"  ,
+	  'vg'  : "#B23AEE"  ,
+	   'a'  : "#deffac"  ,
+	   'ad' : "#8B795E"  ,
+	  'vshi': "#8B6969"  ,
+	   'ag' : "#7EC0EE"  ,
+	   'al' : "#7CFC00"  ,
+	   'b'  : "#ffb5b5"  ,
+	   'bl' : "#6B8E23"  ,
+	   'z'  : "#e2c2de"  ,
+	   'r'  : "#00EEEE"  ,
+	   'rr' : "#c1ffe4"  ,
+	   'vz' : "#c1ffe4"  ,
+	   'rzt': "#008B8B"  ,
+	   'rzs': "#00688B"  ,
+	   'rzv': "#27408B"  ,
+	   'ry' : "#218868"  ,
+	   'ryt': "#20B2AA"  ,
+	   'rys': "#2E8B57"  ,
+	   'ryv': "#228B22"  ,
+	   'rg' : "#218868"  ,
+	   'rz' : "#548B54"  ,
+	   'm'  : "#6C7E92"  ,
+	   'mq' :"#0000CD"   ,
+	   'q'  : "#a3d1d1"  ,
+	   'qv' : "#0000EE"  ,
+	   'qt' : "#ca8eff"  ,
+	   'd'  : "#b8b8dc"  ,
+	   'p'  : "#ffd9ec"  ,
+	   'pba': "#00FA9A"  ,
+	   'pbei': "#00F5FF" ,
+	   'c'  : "#ffe6d9"  ,
+	   'cc' : "#00EE76"  ,
+	   'u'  : "#d9b3b3"  ,
+	   'ul' : "#d9b3b9"  ,
+	   'uzhe': "#00E5EE" ,
+	   'ule' : "#00CED1" ,
+	   'uguo': "#00CD66" ,
+	   'ude1': "#8B2252" ,
+	   'ude2': "#8B008B" ,
+	   'ude3': "#8968CD" ,
+	   'usuo': "#878787" ,
+	  'udeng': "#838B8B" ,
+	   'uyy' : "#7FFFD4" ,
+	   'udh' : "#7D9EC0" ,
+	   'uls' : "#7CCD7C" ,
+	   'uzhi': "#7A7A7A" ,
+	  'ulian': "#708090" ,
+	   'uj'  : "#8080FF" ,
+	   'uv'  : "#B0E11E" ,
+	   'uz'  : "#00FF80" ,
+	   'i'   : "#408080" ,
+	   'j'   : "#566CA9" ,
+	   'ug'  : "#408080" ,
+	   'dg'  : "#408080" ,
+	   'e'   : "#cdcd9a" ,
+	   'y'   : "#9f35ff" ,
+	   'o'   : "#ffed97" ,
+	   'h'   : "#ffbfff" ,
+	   'k'   : "#84c1ff" ,
+	   'x'   : "#bbffbb" ,
+	   'xs'  : "#000080" ,
+	   'xm'  : "#008B45" ,
+	   'xu'  : "#006400" ,
+	   'xe'  : "#EE2C2C" ,
+	   'w'   : "#bebebe" ,
+	   'wkz' : "#6495ED" ,
+	   'wky' : "#607B8B" ,
+	   'wyz' : "#5CACEE" ,
+	   'wyy' : "#575757" ,
+	   'wj'  : "#528B8B" ,
+	   'ww'  : "#483D8B" ,
+	   'wt'  : "#458B74" ,
+	   'wd'  : "#436EEE" ,
+	   'wf'  : "#3A5FCD" ,
+	   'wn'  : "#2F4F4F" ,
+	   'wm'  : "#20B2AA" ,
+	   'ws'  : "#1C86EE" ,
+	   'wp'  : "#1874CD" ,
+	   'wb'  : "#104E8B" ,
+	   'wh'  : "#00FF7F" ,
+'userDefine' : "#0080FF"
+	}
+    df_word['color']=list(map(lambda x:color[x],df_word['class']))
+    df_word['ind']=list(range(len(df_word)))
+    return df_word
+
+def dict_df(df_word):
+    """
+    返回要求的字典形式
+    :param df_word: 词频、词性数据框
+    :return: 返回字典形式
+    """
+    df_dict={}
+    for i in range(len(df_word)):
+        x=df_word.loc[i,:]
+        if x['ind'] not in df_dict:
+            df_dict[x['ind']]={}
+            df_dict[x['ind']]={'name':x['out'] ,'itemStyle': {'normal':{'colour':x['color']}},'value':1000-x['ind']}
+
+    return df_dict
+
+
+
+
+################################################ 180115_new end #########################################################################
 '''
 词性标注功能
 '''
